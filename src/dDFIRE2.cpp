34e0767a30d735894d2711663d52992e66d63c1a
Nima Sarang <nimasarang@gmail.com>
2018-11-16 01:28:28 +0330

    DFIRE2 done

--- src/dDFIRE2.cpp
+++ src/dDFIRE2.cpp
@@ -2,25 +2,35 @@
 
 
 // dDFIRE
-std::unordered_map<std::string, int> atom_map;
-double edDFIRE[MAX_TYPES][MAX_TYPES][MAX_BIN];
-int num_types, num_bins;
+
+DFIRE2::DFIRE2(std::string libPath) {
+	ReadLib(libPath);
+}
 
 
-void dDFIRE_ReadLib(std::string libPath) {
+
+void DFIRE2::ReadLib(std::string libPath) {
 	if (!FileExists(libPath))
 		throw std::runtime_error("ERR: dDFIRE energy file not found!");
 
 	std::string HEAD, AA_name1, AA_name2,
 	    atom1, atom2,
 	    atomtype1, atomtype2;
-	int count = 0,
+
+	int num_types, num_bins,
+		count_now = 0, // for printing
 	    pos = 1, // zero is reserved for unknown residues
-	    total;
+	    total; // total atom pairs
 
 	std::ifstream libFile(libPath);
 	libFile >> HEAD >> num_types >> HEAD >> num_bins;
 	total = num_types * (num_types + 1) / 2;
+	
+	nTypes = num_types; // set class variables
+	nBins = num_bins;
+	// 3D array  [nTypes * nTypes * nBins]
+	edDFIRE = std::vector<std::vector<std::vector<double> > >(nTypes+1, std::vector<std::vector<double> >(nTypes+1, std::vector<double>(nBins+1)));
+
 
 	while (libFile >> AA_name1 >> atom1 >> AA_name2 >> atom2) {
 		atomtype1 = std::string() + residue_map[AA_name1] + " " + atom1;
@@ -40,18 +50,19 @@ void dDFIRE_ReadLib(std::string libPath) {
 			libFile >> edDFIRE[id1][id2][i];     // 30 bins of energy
 			edDFIRE[id2][id1][i] = edDFIRE[id1][id2][i];  // symmetry
 		}
-		progress_indicator("dDFIRE energy file", count++, total);
+		progress_indicator("dDFIRE energy file", count_now++, total);
 	}
 	progress_indicator("dDFIRE energy file", 1, 1);
+	libReady = true;
 }
 
 
 
-double dDFIRE_CFE(std::string& target_sequence,
+double DFIRE2::Calculate_CFE(std::string& target_sequence,
                   std::array<std::array<double, MAX_PROT_LEN>, MAX_PROT_LEN> &target_atom_distance) {
 	double eAll = 0;
 	int n = target_sequence.size();
-	std::array<int, MAX_PROT_LEN> atom_id;
+	std::vector<int> atom_id( n );
 
 	for (int i = 0; i < n; ++i) {
 		std::string key = std::string() + target_sequence[i] + " CA"; // char wont concant to string!
@@ -61,7 +72,7 @@ double dDFIRE_CFE(std::string& target_sequence,
 	for (int i = 0; i < n; ++i) {
 		for (int j = i + 1; j < n; ++j) {
 			int b = target_atom_distance[i][j] * 2;     // distance to bin
-			if (b >= num_bins)
+			if (b >= nBins)
 				continue;
 			eAll += edDFIRE[atom_id[i]][atom_id[j]][b];
 		}
