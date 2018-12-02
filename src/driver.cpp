286c77feff3f4d1fad2d9380e16645a05820753e
Nima Sarang <nimasarang@gmail.com>
2018-12-02 19:35:10 +0330

    Updated driver.

--- src/driver.cpp
+++ src/driver.cpp
@@ -1,5 +1,7 @@
 
 #include <iostream>
+#include <algorithm>
+#include <random>
 #include "mapping.h"
 #include "utility.h"
 #include "constants.h"
@@ -7,22 +9,33 @@
 #include "protein.h"
 #include "protein_profile.h"
 #include "evaluator.h"
+#include "subprocess.hpp"
 
-
+using namespace std;
 
 
 
 int main(int argc, const char * argv[]) {
-    assert(argc > 1);
-    std::string target_path = argv[1];
+	assert(argc > 1);
+	std::string target_path = argv[1];
+	auto target = Protein(target_path, 1 + 2 + 4 + 8);
 
-    auto target = Protein(target_path, 1 + 2 + 4 + 8);
-    std::cerr << target.Get_Sequence() << "\n";
+	auto vecDB = utility::CATH_ListFiles(db_CATH);
+	
+	/*
+	decltype(vecDB) vecSample;
+	std::sample(vecDB.begin(), vecDB.end(),
+	            std::back_inserter(vecDB1),
+	            1000, std::mt19937{std::random_device{}()});
+	*/
 
-    auto profile = ProteinProfile(target);
+	for (auto fPath : vecDB) {
+		std::cerr << fPath << "\n";
+		auto prot = Protein(fPath);
+		auto profile = ProteinProfile(prot);
 
-    auto vecDB = utility::CATH_ListFiles(db_CATH);
-    profile.Find_Homologous_Proteins(vecDB, DIST_CUTOFF, true);
-    profile.CalculateProfiles(1 + 2 + 4 + 8, true);
-    profile.Write_ToFile(true);
+		profile.Find_Homologous_Proteins(vecDB, true, ALIGN_SC_CUTTOFF);
+		profile.CalculateProfiles(1 + 2 + 4 + 8, true);
+		profile.Write_ToFile(true);
+	}
 }
