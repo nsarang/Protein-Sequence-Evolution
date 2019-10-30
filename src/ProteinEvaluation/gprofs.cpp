c30258ee02fc51beb301d180ccb28d93ecb950a4
Nima Sarang <nimasarang@gmail.com>
2019-10-31 00:54:17 +0330

    Add driver to generate profiles

---
+++ src/ProteinEvaluation/gprofs.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <random>
+#include "mapping.h"
+#include "utility.h"
+#include "constants.h"
+#include "statistics.h"
+#include "protein.h"
+#include "protein_profile.h"
+#include "evaluator.h"
+#include "deep.h"
+using namespace std;
+
+
+int main(int argc, const char * argv[]) {
+	// assert(argc > 1);
+	// std::string target_path = argv[1];
+	// auto target = Protein(target_path, 1 + 2 + 4 + 8);
+
+
+	auto vecDB = utility::CATH_ListFiles(db_CATH);
+	
+	/*
+	decltype(vecDB) vecSample;
+	std::sample(vecDB.begin(), vecDB.end(),
+	            std::back_inserter(vecDB1),
+	            1000, std::mt19937{std::random_device{}()});
+	*/
+
+	for (auto fPath : vecDB) {
+		std::cerr << fPath << "\n";
+		auto prot = Protein(fPath);
+		auto profile = ProteinProfile(prot);
+
+		profile.Find_Homologous_Proteins(vecDB);
+		profile.CalculateProfiles(1 + 2 + 4 + 8);
+		profile.Write_ToFile();
+	}
+}
