0a882439d95ee97f574376d92c3e3a6aec3c2ba1
Nima Sarang <nimasarang@gmail.com>
2020-01-26 06:35:18 +0330

    Increase number of threads

--- src/ProteinEvaluation/dataset.cpp
+++ src/ProteinEvaluation/dataset.cpp
@@ -119,7 +119,7 @@ void Dataset::GenerateDataset(std::string sFamDir,
 	std::ofstream trFile(fCSV);
 	trFile.close(); // Truncate file
 	auto vecDB = utility::CATH_ListFiles(db_CATH);
-	ThreadPool pool(std::thread::hardware_concurrency()); // Creates threads.
+	ThreadPool pool(2 * std::thread::hardware_concurrency()); // Creates threads.
 
 	std::map<int, std::vector<Protein>> mpProtEqLen;
 	int countNow = 0;
